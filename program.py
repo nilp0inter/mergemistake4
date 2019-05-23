150cbc97ab512aad2e6430b97a5f34548254439a
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-05-23 08:54:49 +0200

    Base program.

---
+++ program.py
@@ -0,0 +1,9 @@
+if user.is_friend():
+    # Friend behavior
+    user.greet()  # Good manners after all
+    user.say_goodbye()
+
+if user.is_enemy():
+    # Enemy behavior
+    user.greet()  # Good manners after all
+    user.say_goodbye()
