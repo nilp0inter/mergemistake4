739e062a7982eaff889d2dcc32f33bb22fda2afc
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-05-23 08:56:44 +0200

    Customize user behavior.

--- program.py
+++ program.py
@@ -1,9 +1,11 @@
 if user.is_enemy():
     # Enemy behavior
     user.greet()  # Good manners after all
+    user.stab_to_death()
     user.say_goodbye()
 
 if user.is_friend():
     # Friend behavior
     user.greet()  # Good manners after all
+    user.hug_with_love()
     user.say_goodbye()
